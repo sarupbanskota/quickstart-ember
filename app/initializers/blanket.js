7cbc5eaf7d345890766e20817ce70f80c080193f
Sarup Banskota <sbanskota08@gmail.com>
2017-09-23 23:39:57 +0700

    Introduce atom style notifications

--- app/initializers/blanket.js
+++ app/initializers/blanket.js
@@ -2,6 +2,17 @@ export function initialize(application) {
   application.inject("route", "session", "service:session");
   application.inject("component", "session", "service:session");
   application.inject("controller", "session", "service:session");
+
+  application.inject(
+    "component",
+    "notifications",
+    "service:notification-messages"
+  );
+  application.inject(
+    "controller",
+    "notifications",
+    "service:notification-messages"
+  );
 }
 
 export default {
